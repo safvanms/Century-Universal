c00575a5852fdf28d14804a8c1eb899eff6ae8f8
safvanms <muhammedsafvan1ms@gmail.com>
2022-10-28 15:34:53 +0530

    env updated

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:"cu",cookie : { maxAge:600000000}}))
+app.use(session({secret:"cu",cookie : { maxAge:86400000}}))
 app.use(nocache());
 
 
